0c31aa0f12e6d24e4c8c1a690490aaa07ea59ce3
Colin Dean <cad@cad.cx>
2011-10-04 22:07:47 -0400

    initial commit, README-driven development ftw

---
+++ test/fillertext_test.rb
@@ -0,0 +1,27 @@
+require 'minitest/autorun'
+require 'fillertext'
+
+class FillerTextTest < MiniTest::Unit::TestCase
+
+  def setup
+
+  end
+
+  test "should generate some text"
+
+    FillerText.sentences 5
+    FillerText.words 2
+    FillerText.characters 5
+    FillerText.bytes 4
+    FillerText.paragraphs 1
+
+    2.filler.sentences
+    1.filler.paragraphs
+
+
+    FillerText.style = FillerText::LoremIpsum
+    FillerText.style = FillerText::HipsterIpsum
+    FillerText.style = FillerText::YinzerIpsum
+  end
+	
+end
