4e9d614b1f05b6190ab64b96203b579e0410d277
Colin Dean <git@cad.cx>
2012-08-08 14:52:39 -0300

    Additional Pittsburghese phrases

--- lib/fillertext/styles/yinzer.rb
+++ lib/fillertext/styles/yinzer.rb
@@ -1,7 +1,13 @@
 module FillerText::Style
   class YinzerIpsum
     def self.text
-      "Yinzer ipsum stillers dahntahn Picksburgh. Jeet jet? No, joo? Wawrsh yer Primanti's dahn with Imp an' arn."
+      "Yinzer ipsum, yinz jagoffs. Dem Stillers ain't in dahntahn Picksburgh, and da Buccos aren't there, either. The Civic Arena was a beaut', but nah the Pens got that Consol Center thing that no one can pronounce. 
+      Jeet jet? No, joo? Wawrsh yer Primanti's dahn with Imp an' arn. She likes kolbassi with Heinz ketchup 'n' 'at on it.
+      My mom put on her babushka. My sister teased me because I couldn't go to Kennywood because my room needs redding up. She's a nebby jagoff.
+      I wanna go to that frozen yogurt place where they've got jimmies comin' out of a spicket on the wall. It's not on Mount Wawrshington.
+      She's got that neat baby buggy with the hap on the back, held on by a gumband. She runs 'round the Sah Side while he works in some office in S'Liberty.
+      Breakfast needed made, so I threw together some chipped ham on a hoagie bun. She yelled at me because I didn't make dippy eggs. I put white pepper in his cruddled milk. That'll show 'im! Anymore, we're always jaggin' each other like this. Keeps things interesting.
+      That damn grinnie just fell off the bannister. He must not have seen the weather! It's slippy aht!"
     end
   end
 end
