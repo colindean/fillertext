fdfd902534cf7ade74b9ff7ad8ef25e3170f4f8a
Colin Dean <git@cad.cx>
2021-10-23 01:42:16 +0000

    Uses Minitest _(value) wrapper to prepare for Minitest 6
    Closes #26

--- spec/fillertext_spec.rb
+++ spec/fillertext_spec.rb
@@ -16,29 +16,29 @@ describe FillerText do
     it "should output sentences" do
       f = FillerText.sentences 5
       #look for the periods
-      f.scan(/\./).size.must_equal 4
+      _(f.scan(/\./).size).must_equal 4
     end
 
     it "should output words" do
       f = FillerText.words 5
       #look for four spaces
-      f.must_equal "Lorem ipsum dolor sit amet"
+      _(f).must_equal "Lorem ipsum dolor sit amet"
     end
 
     it "should output characters" do
       f = FillerText.characters 5
-      f.must_equal "Lorem"
+      _(f).must_equal "Lorem"
     end
 
     it "should output bytes" do
       f = FillerText.bytes 5
-      f.must_equal "Lorem"
+      _(f).must_equal "Lorem"
     end
 
     it "should output paragraphs" do
       f = FillerText.paragraphs 5
       #look for newlines
-      f.scan(/\n/).size.must_equal 4
+      _(f.scan(/\n/).size).must_equal 4
     end
   end
 
@@ -52,29 +52,29 @@ describe FillerText do
     it "should output sentences" do
       f = 5.filler.sentences
       #look for the periods
-      f.scan(/\./).size.must_equal 4
+      _(f.scan(/\./).size).must_equal 4
     end
 
     it "should output words" do
       f = 3.filler.words
       #look for four spaces
-      f.must_equal "Lorem ipsum dolor"
+      _(f).must_equal "Lorem ipsum dolor"
     end
 
     it "should output characters" do
       f = 5.filler.characters
-      f.must_equal "Lorem"
+      _(f).must_equal "Lorem"
     end
 
     it "should output bytes" do
       f = 5.filler.bytes
-      f.must_equal "Lorem"
+      _(f).must_equal "Lorem"
     end
 
     it "should output paragraphs" do
       f = 5.filler.paragraphs
       #look for newlines
-      f.scan(/\n/).size.must_equal 4
+      _(f.scan(/\n/).size).must_equal 4
     end
   end
 
@@ -84,19 +84,19 @@ describe FillerText do
     it "should use LoremIpsum" do
       FillerText::FillerText.style = FillerText::Style::LoremIpsum
       t = "Lorem ipsum"
-      t.length.filler.characters.must_equal t
+      _(t.length.filler.characters).must_equal t
     end
 
     it "should use hipster ipsum" do
       FillerText::FillerText.style = FillerText::Style::HipsterIpsum
       t = "Hipster ipsum"
-      t.length.filler.characters.must_equal t
+      _(t.length.filler.characters).must_equal t
     end
 
     it "should use yinzer ipsum" do
       t = "Yinzer ipsum"
       FillerText::FillerText.style = FillerText::Style::YinzerIpsum
-      t.length.filler.characters.must_equal t
+      _(t.length.filler.characters).must_equal t
     end
 
     #add more styles here
