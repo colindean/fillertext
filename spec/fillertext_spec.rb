31f30b45935ff75284847c8393b68402a3107b0a
Colin Dean <git@cad.cx>
2021-10-23 01:39:43 +0000

    Fixes whitespace in spec

--- spec/fillertext_spec.rb
+++ spec/fillertext_spec.rb
@@ -5,7 +5,7 @@ SimpleCov.start if ENV['COVERAGE']
 require_relative '../lib/fillertext'
 
 describe FillerText do
-  
+
   describe "when explicity called" do
     include FillerText
 
@@ -24,24 +24,24 @@ describe FillerText do
       #look for four spaces
       f.must_equal "Lorem ipsum dolor sit amet"
     end
-    
+
     it "should output characters" do
-      f = FillerText.characters 5    
+      f = FillerText.characters 5
       f.must_equal "Lorem"
     end
-    
+
     it "should output bytes" do
       f = FillerText.bytes 5
       f.must_equal "Lorem"
     end
-    
+
     it "should output paragraphs" do
       f = FillerText.paragraphs 5
       #look for newlines
       f.scan(/\n/).size.must_equal 4
     end
   end
-  
+
   describe "when called from an integer" do
     include FillerText
 
@@ -50,34 +50,34 @@ describe FillerText do
     end
 
     it "should output sentences" do
-      f = 5.filler.sentences 
+      f = 5.filler.sentences
       #look for the periods
-      f.scan(/\./).size.must_equal 4  
+      f.scan(/\./).size.must_equal 4
     end
 
     it "should output words" do
-      f = 3.filler.words  
+      f = 3.filler.words
       #look for four spaces
       f.must_equal "Lorem ipsum dolor"
     end
-    
+
     it "should output characters" do
-      f = 5.filler.characters 
-      f.must_equal "Lorem"  
-    end
-    
-    it "should output bytes" do
-      f = 5.filler.bytes  
+      f = 5.filler.characters
       f.must_equal "Lorem"
     end
-    
+
+    it "should output bytes" do
+      f = 5.filler.bytes
+      f.must_equal "Lorem"
+    end
+
     it "should output paragraphs" do
       f = 5.filler.paragraphs
       #look for newlines
-      f.scan(/\n/).size.must_equal 4 
+      f.scan(/\n/).size.must_equal 4
     end
   end
-  
+
   describe "when changing filler text style" do
     include FillerText
 
@@ -86,13 +86,13 @@ describe FillerText do
       t = "Lorem ipsum"
       t.length.filler.characters.must_equal t
     end
-  
+
     it "should use hipster ipsum" do
       FillerText::FillerText.style = FillerText::Style::HipsterIpsum
       t = "Hipster ipsum"
       t.length.filler.characters.must_equal t
     end
-    
+
     it "should use yinzer ipsum" do
       t = "Yinzer ipsum"
       FillerText::FillerText.style = FillerText::Style::YinzerIpsum
@@ -100,7 +100,7 @@ describe FillerText do
     end
 
     #add more styles here
-    
-  end    
-    
+
+  end
+
 end
