8918e3f20477c6fc7b3750f68cea04300b302570
Colin Dean <colindean@users.noreply.github.com>
2021-10-24 18:56:20 -0400

    Adds a reference to the Pittsburgh downtown bus hole
    https://www.nbcnews.com/news/us-news/surreal-images-show-city-bus-swallowed-sinkhole-downtown-pittsburgh-n1072976

--- lib/fillertext/styles/yinzer.rb
+++ lib/fillertext/styles/yinzer.rb
@@ -7,7 +7,8 @@ module FillerText::Style
       I wanna go to that frozen yogurt place where they've got jimmies comin' out of a spicket on the wall. It's not on Mount Wawrshington.
       She's got that neat baby buggy with the hap on the back, held on by a gumband. She runs 'round the Sah Side while he works in some office in S'Liberty.
       Breakfast needed made, so I threw together some chipped ham on a hoagie bun. She yelled at me because I didn't make dippy eggs. I put white pepper in his cruddled milk. That'll show 'im! Anymore, we're always jaggin' each other like this. Keeps things interesting.
-      That damn grinnie just fell off the bannister. He must not have seen the weather! It's slippy aht!"
+      That damn grinnie just fell off the bannister. He must not have seen the weather! It's slippy aht!
+      Jinz see 'at city buss fell down in 'at hole dahntahn? It had its blinkers on like it was just stoppin' for a moment! It was dahn in the soor. Roads 'n'at needs fixed."
     end
   end
 end
