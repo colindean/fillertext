390cb0d8867f372a9555acec798a3b258341c36d
Ashvith <ashvithshetty10@gmail.com>
2022-01-26 14:45:18 +0530

    Update spec/spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
+require "simplecov"
 require "fillertext"
+
+SimpleCov.start
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
