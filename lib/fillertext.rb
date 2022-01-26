5ec0b7f103d923814071a9c29f741fa64a53803e
Ashvith <ashvithshetty10@gmail.com>
2022-01-26 13:35:45 +0530

    Update lib/fillertext.rb

--- lib/fillertext.rb
+++ lib/fillertext.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "fillertext/fillertext"
-require_relative "fillertext/fixnum"
+require_relative "fillertext/integer"
 require_relative "fillertext/version"
 
 # Generates filler/placeholder text in a Rubyistic way
