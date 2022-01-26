a37bd9cbd1cfa09c112870dea0619dd7a742ffeb
Ashvith <ashvithshetty10@gmail.com>
2022-01-26 14:47:48 +0530

    Update spec/explicit_spec.rb

--- spec/explicit_spec.rb
+++ spec/explicit_spec.rb
@@ -2,36 +2,25 @@
 
 RSpec.describe FillerText do
   describe "when explicitly called" do
-    # include FillerText
-    before do
-      FillerText.style = FillerText::Style::LoremIpsum
-    end
+    before { FillerText.style = FillerText::Style::LoremIpsum }
     it "should output sentences" do
-      f = FillerText.sentences 5
-      # look for the periods
-      expect(f.scan(/\./).size).to eql 4
+      expect(FillerText.sentences(5).scan(/\./).size).to eql 4
     end
 
     it "should output words" do
-      f = FillerText.words 5
-      # look for four spaces
-      expect(f).to eql "Lorem ipsum dolor sit amet"
+      expect(FillerText.words(5)).to eql "Lorem ipsum dolor sit amet"
     end
 
     it "should output characters" do
-      f = FillerText.characters 5
-      expect(f).to eql "Lorem"
+      expect(FillerText.characters(5)).to eql "Lorem"
     end
 
     it "should output bytes" do
-      f = FillerText.bytes 5
-      expect(f).to eql "Lorem"
+      expect(FillerText.bytes(5)).to eql "Lorem"
     end
 
     it "should output paragraphs" do
-      f = FillerText.paragraphs 5
-      # look for newlines
-      expect(f.scan(/\n/).size).to eql 4
+      expect(FillerText.paragraphs(5).scan(/\n/).size).to eql 4
     end
   end
 end
