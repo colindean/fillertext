c0049803405852753d9a0408d280cca52ca66fdb
Colin Dean <colindean@users.noreply.github.com>
2023-10-31 23:32:17 -0400

    Create scots_word_of_the_day.rb as homage to Len Pennie
    https://en.wikipedia.org/wiki/Len_Pennie

---
+++ lib/fillertext/styles/scots_word_of_the_day.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module FillerText
+  module Style
+    # Style: MikeLange
+    class ScotsWordOfTheDay
+      # https://www.instagram.com/misspunnypennie/
+      def self.text
+        "Thon tumshie/baigie/neep gied him a fleg.
+            Dinnae ye gang oot aifter the mirkenin.
+            She's ma neebour.
+            A'm aff the morra.
+            Whaur ye gaun?
+            Och, hameldaeme.
+            She's aye hail at the hert.
+            Dinnae loss the heid wae me!
+            Thon lassie had muckle powstie.
+            He's a bauld-dar laddie.
+            The wean's maw telt him, 'Yer tea's oot.'"
+      end
+    end
+  end
+end
